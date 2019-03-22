613c7a06b229bf7af079d135944619f63aefbb14
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2019-03-22 09:34:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+import Stats from './Stats';
+import Burger from './Burger';
+import Booster from './Booster';
+import Menu from './Menu';
+
+import './App.css';
+
+class Clicker extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0
+    }
+    this.mouseClicked = this.mouseClicked.bind(this);
+  }
+
+  mouseClicked() {
+    const clicks = this.state.clicks;
+    this.setState({
+      clicks: clicks + 1
+    });
+  }
+
+  render() {
+    return (
+      <div className="clicker">  
+        <div className="header">
+          <h1>Burger Clicker</h1>
+        </div>
+        <div className="content content--justified">
+          <Stats count={this.state.clicks} />
+          <Burger onClick={this.mouseClicked} />
+          <Booster boost={3.2}/> 
+        </div>           
+        <Menu claimableCoupons={5} />      
+      </div>
+    );
+  }
+}
+
+export default Clicker;
